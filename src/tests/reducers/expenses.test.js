e02802d5ef7d97ad6e2d49a95c75b71cbe66f7c7
Kayo Filipe Oliveira Magrini Silva <kayofilipe@hotmail.com>
2017-09-24 23:20:27 -0300

    Initial commit

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,65 @@
+import expensesReducer from '../../reducers/expenses';
+import expenses from '../fixtures/expenses';
+
+test('should set default state',() => {
+    const state = expensesReducer(undefined, { type: '@@INIT' });
+    expect(state).toEqual([]);
+});
+
+test('should remove expense by id', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: expenses[1].id
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([expenses[0], expenses[2]]);
+});
+
+test('should not remove expense by id', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: '-1'
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual(expenses);
+});
+
+test('should add expense', () => {
+    const expense = {
+        id: '4',
+        description: 'Test',
+        note: '',
+        amount: 1,
+        createdAt: 0
+    };
+    const action = {
+        type: 'ADD_EXPENSE',
+        expense
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([...expenses, expense]);
+});
+
+test('should edit expense', () => {
+    const action = {
+        type: 'EDIT_EXPENSE',
+        id: expenses[0].id,
+        updates: {
+            note: 'teste'
+        }
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state[0].note).toEqual(action.updates.note);
+});
+
+test('should not edit expense if expense not found', () => {
+    const action = {
+        type: 'EDIT_EXPENSE',
+        id: '-1',
+        updates: {
+            note: 'teste'
+        }
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual(expenses);
+});
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }, {
+            test: /\.s?css$/,
+            use: [
+                'style-loader', //dump the css into the tag style
+                'css-loader', //read the css file
+                'sass-loader' //convert the scss file to css
+            ]
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true
+    }
+};
