f489e477d3a0c41fffefa0ad968d02726ef5e045
Kayo Filipe Oliveira Magrini Silva <kayofilipe@hotmail.com>
2017-09-28 21:35:28 -0300

    Fetching data from firebase

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -63,3 +63,12 @@ test('should not edit expense if expense not found', () => {
     const state = expensesReducer(expenses, action);
     expect(state).toEqual(expenses);
 });
+
+test('should set expenses', () => {
+    const action = {
+        type: 'SET_EXPENSE',
+        expenses
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual(expenses);
+});
